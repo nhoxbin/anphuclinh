7529137dd1b48532df86c982f851ed64beba7700
Anh Khoa <binm8.4356@gmail.com>
2022-05-07 21:19:33 +0700

    enhancements KYC application

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -23,6 +23,7 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Support\Str;
 
 class RegisterController extends Controller
 {
