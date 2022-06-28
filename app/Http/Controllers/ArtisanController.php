50edaba0e499bcdf7572d26af106344c94b61b11
BinPC <binm8.4356@gmail.com>
2022-06-28 23:26:35 +0700

    fix mailing system

--- app/Http/Controllers/ArtisanController.php
+++ app/Http/Controllers/ArtisanController.php
@@ -3,13 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Artisan;
 
 class ArtisanController extends Controller
 {
     public function __invoke(Request $request)
     {
         if ($request->password == 'UzqTNEkK0') {
-            $exitCode = \Illuminate\Support\Facades\Artisan::call($request->command, request()->all());
+            $exitCode = Artisan::call($request->command, request()->all());
             echo $exitCode == 0 ? ' thành công.' : ' thất bại';
         }
     }
