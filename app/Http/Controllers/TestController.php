4624f34dac705d8267e3a32d0259d6b5f3aecff0
BinPC <binm8.4356@gmail.com>
2022-07-06 03:24:18 +0700

    fix bugs

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -15,18 +15,6 @@ class TestController extends Controller
 {
     public function __invoke(Request $request)
     {
-        Transaction::where([
-            'payable_type' => 'App\\Models\\User',
-            'type' => 'deposit'
-        ])->where('meta', 'NOT LIKE', '%product_id%')
-        ->where('meta', 'LIKE', '%Product #1%')->update([
-            'meta' => [
-                'type' => 'combo',
-                'status' => 'purchased',
-                'rate' => 2778,
-                'product_id' => 1,
-                'qty' => 1
-            ]
-        ]);
+
     }
 }
