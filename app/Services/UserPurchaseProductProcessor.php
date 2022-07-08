779ff45fd51040e0ad30e3f457f96072bcbebcd0
BinPC <binm8.4356@gmail.com>
2022-07-08 14:10:46 +0700

    fix bugs

--- app/Services/UserPurchaseProductProcessor.php
+++ app/Services/UserPurchaseProductProcessor.php
@@ -156,11 +156,7 @@ class UserPurchaseProductProcessor
             // trừ hoa hồng (bonus)
             $sales = Transaction::with('payable')->where(['type' => 'bonus', 'meta->transaction_id' => $transaction->id])->get();
             $sales->map(function($t) use ($data) {
-                if ($t->payable->balance >= $t->amount) {
-                    $t->payable->withdraw($t->amount, $data);
-                } elseif ($t->payable->balance > 0) {
-                    $t->payable->forceWithdraw($t->amount, $data);
-                }
+                $t->payable->forceWithdraw($t->amount, $data);
             });
             // refund
             $user->deposit($transaction->amount, $data);
